af5e05db31700ef0118f093edb37a42f02bfa925
ChrisMtz505 <cristoelpipo@gmail.com>
2024-10-16 19:22:43 -0600

    2

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -8,11 +8,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,41 +49,25 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.project1.ui.screens.ComponentsScreen
-import com.example.project1.ui.screens.HomeScreen
-import com.example.project1.ui.screens.MenuScreen
+import com.example.project1.ui.theme.Project1Theme
+import com.example.project1.ui.sreens.Components
+import com.example.project1.ui.sreens.HomeScreen
+import com.example.project1.ui.sreens.MenuScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MultiScreenNavigationApp()
+            ComposeMultiScreenApp()
         }
     }
 }
 
 @Composable
-fun MultiScreenNavigationApp() {
-    val controller = rememberNavController()
-    Surface(color = Color.White) {
-        SetupNavigation(controller)
-    }
-}
-
-@Composable
-fun SetupNavigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "menu") {
-        composable("menu") { MenuScreen(navController) }
-        composable("home") { HomeScreen(navController) }
-        composable("components") { ComponentsScreen(navController) }
-    }
-}
-
-@Composable
-fun CustomGreeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hi $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
@@ -80,11 +75,79 @@ fun CustomGreeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CustomGreeting("Uriel")
+    Project1Theme {
+        Greeting("Uriel")
+    }
 }
 
 @Composable
-fun SampleCard1() {
+fun ModifierExample() {
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+    ) {
+        Text(text = "Hello world")
+    }
+}
+
+@Composable
+fun ModifierExample3() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(Color.Magenta)
+            .border(width = 2.dp, color = Color.Blue)
+            .width(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Item 1")
+        Text(text = "Item 2")
+        Text(text = "Item 3")
+        Text(text = "Item 4")
+        Text(text = "Item 5")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomText() {
+    Column {
+        Text(
+            stringResource(R.string.hello_world_text),
+            color = colorResource(R.color.teal_700),
+            fontSize = 28.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Thin
+        )
+        val gradientColors = listOf(Cyan, Blue)
+        Text(
+            stringResource(R.string.hello_world_text),
+            style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+        )
+    }
+}
+
+@Composable
+fun Picture() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(Color.Black)
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.android_logo),
+            contentDescription = "Android logo",
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
+fun Content1() {
     Card(
         modifier = Modifier
             .background(Color.LightGray)
@@ -92,7 +155,7 @@ fun SampleCard1() {
             .padding(5.dp)
     ) {
         Text(
-            text = "Title Example",
+            text = "This is a title",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
@@ -113,5 +176,109 @@ fun SampleCard1() {
             modifier = Modifier
                 .padding(10.dp)
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Content2() {
+    Card(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Column {
+            Row {
+                Image(
+                    modifier = Modifier
+                        .height(150.dp),
+                    painter = painterResource(id = R.drawable.android_logo),
+                    contentDescription = "Android Logo",
+                    contentScale = ContentScale.Crop
+                )
+                Column {
+                    Text(
+                        text = "This is a title",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(10.dp)
+                    )
+                    Text(
+                        stringResource(R.string.text_card),
+                        textAlign = TextAlign.Justify,
+                        lineHeight = 10.sp,
+                        modifier = Modifier
+                            .padding(10.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxExample1() {
+    Box(
+        modifier = Modifier
+            .background(Color.DarkGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Image(
+            painterResource(R.drawable.android_logo),
+            contentDescription = "Android Logo",
+            contentScale = ContentScale.FillBounds
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 150.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                Icons.Filled.AccountCircle,
+                contentDescription = "Icon Account"
+            )
+            Text(text = "Text")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxExample2() {
+    Box(
+        modifier = Modifier
+            .background(Color.Magenta)
+            .padding(5.dp)
+            .size(250.dp)
+    ) {
+        Text(text = "TopStart", Modifier.align(Alignment.TopStart))
+        Text(text = "TopEnd", Modifier.align(Alignment.TopEnd))
+        Text(text = "CenterStart", Modifier.align(Alignment.CenterStart))
+        Text(text = "Center", Modifier.align(Alignment.Center))
+        Text(text = "CenterEnd", Modifier.align(Alignment.CenterEnd))
+        Text(text = "BottomStart", Modifier.align(Alignment.BottomStart))
+        Text(text = "BottomEnd", Modifier.align(Alignment.BottomEnd))
+    }
+}
+
+@Composable
+fun ComposeMultiScreenApp() {
+    val navController = rememberNavController()
+    Surface(color = Color.White) {
+        SetupNavGraph(navController = navController)
+    }
+}
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "menu") {
+        composable("menu") { MenuScreen(navController) }
+        composable("home") { HomeScreen(navController) }
+        composable("componentes") { Components(navController) }
     }
 }
